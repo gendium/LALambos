75baf2e6a970d118a23d0e44f0018c841e127928
Sean Egger <eggersean@gmail.com>
2017-12-07 00:23:42 -0800

    added source files for cars and addons

---
+++ src/BHillsCar.java
@@ -0,0 +1,21 @@
+/**
+ Author: Sean Egger
+ Class: CSI-340
+ Assignment: Software Specialties Final Assignment: LA Car rental app
+ Due Date: 12/7/2017 2:00
+
+ Description:
+ A concrete car implementation
+
+ Certification of Authenticity:
+ I certify that this is entirely my own work, except where I have given
+ fully-documented references to the work of others. I understand the definition
+ and consequences of plagiarism and acknowledge that the assessor of this
+ assignment may, for the purpose of assessing this assignment:
+ - Reproduce this assignment and provide a copy to another member of academic
+ - staff; and/or Communicate a copy of this assignment to a plagiarism checking
+ - service (which may then retain a copy of this assignment on its database for
+ - the purpose of future plagiarism checking)
+ */
+public class BHillsCar extends Car {
+}
