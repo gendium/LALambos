c4dde2e2baabd84c0e368c3b5f1fee71cb9d632b
gendium <acehugh@gmail.com>
2017-12-07 01:26:45 -0500

    Class commit
    All of the other files

---
+++ src/ConcSystem.java
@@ -0,0 +1,4 @@
+
+public class ConcSystem extends System {
+
+}
---
+++ src/UI.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UI implements Mediator {
+public List<User> userList = new ArrayList<User>();
+public List<System> sysList = new ArrayList<System>();
+}
