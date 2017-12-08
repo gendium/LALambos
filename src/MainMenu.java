8dddd253b4b47f4f7383828dd124a672f393c696
Sam LoPiccolo <sam.lopiccolo@mymail.champlain.edu>
2017-12-08 07:57:52 -0500

    Update and rename ConcSystem.java to MainMenu.java

---
+++ src/MainMenu.java
@@ -0,0 +1,44 @@
+
+public abstract class MainMenu implements Menu {
+	String name;
+	Menu currentMenu;
+	Map<String, Runnable> menuOptions;
+	
+	public MainMenu()
+	{
+		name = "Main Menu";
+		currentMenu = this;
+		menuOptions = new HashMap<>();
+	}
+	
+	public void addOption(String key, Runnable val)
+	{
+		menuOptions.put(key, () -> val);
+	}
+	
+	public void removeOption(String key)
+	{
+		menuOptions.remove(key);
+	}
+	
+	public void navMenu(Menu newMenu)
+	{
+		currentMenu = newMenu;
+	}
+	
+	public void displayMenu()
+	{
+		while (true) {
+        	System.out.println(name);
+		for(Entry<String,Runnable> entry : menuOptions.entrySet())
+		{
+			String cmd = entry.getKey();
+			System.out.println(cmd);
+		}
+
+        	char key = new Scanner(System.in).nextLine().charAt(0);
+
+        	if (commands.containsKey(key))
+            		commands.get(key).run();
+        }
+}
