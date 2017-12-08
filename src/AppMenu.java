2af308e8d7c015c29266e284d21f2037553e4e42
Sam LoPiccolo <sam.lopiccolo@mymail.champlain.edu>
2017-12-08 07:41:36 -0500

    Update AppMenu.java

--- src/AppMenu.java
+++ src/AppMenu.java
@@ -1,21 +1,57 @@
 
 public abstract class AppMenu implements Menu{
+	String name;
 	MainMenu rootMenu;
 	List<RentalStore> storeList;
-	List<String> menuOptions;
+	Map<String, Runnable> menuOptions;
 	
-	addStore(RentalStore newStore)
+	public AppMenu(MainMenu root)
+	{
+		name = "Application Menu";
+		rootMenu = root;
+		storeList = new List<RentalStore>;
+		menuOptions = new HashMap<>();
+	}
+	
+	public void displayMenu()
+	{
+		while (true) {
+        	System.out.println(name);
+		for(Entry<String,Runnable> entry : menuOptions.entrySet())
+		{
+			String cmd = entry.getKey();
+			System.out.println(cmd);
+		}
+
+        	char key = new Scanner(System.in).nextLine().charAt(0);
+
+        	if (commands.containsKey(key))
+            		commands.get(key).run();
+        }
+		
+	public void addOption(String key, Runnable val)
+	{
+		menuOptions.put(key, val);
+	}
+	
+	public void removeOption(String key)
+	{
+		menuOptions.remove(key);
+	}
+	
+	public void addStore(RentalStore newStore)
 	{
 		storeList.add(newStore);
 	}
 	
-	removeStore(RentalStore oldStore)
+	public void removeStore(RentalStore oldStore)
 	{
 		storeList.remove(oldStore);
 	}
 	
-	quit()
+	public void quit()
 	{
+		System.out.println(rootMenu.name);
 		rootMenu.currentMenu = rootMenu;
 	}
 	
