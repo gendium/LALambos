d0db126bfd3be406c234e30ec24522e12a36ad88
Sam LoPiccolo <sam.lopiccolo@mymail.champlain.edu>
2017-12-08 00:01:03 -0500

    Update Mediator.java

--- src/Mediator.java
+++ src/Mediator.java
@@ -3,7 +3,7 @@ public interface Mediator {
   
     void send_message(String msg, ChatUser user);
 	
-	  void add_user(ChatUser user);
+	void add_user(ChatUser user);
 	
-	  void remove_user(ChatUser user);
+	void remove_user(ChatUser user);
 }
