3bb90cc7e9609b558d190fd424fdb90c144baf79
Sam LoPiccolo <sam.lopiccolo@mymail.champlain.edu>
2017-12-08 08:33:38 -0500

    Update ChatUser.java

--- src/ChatUser.java
+++ src/ChatUser.java
@@ -1,11 +1,13 @@
 public abstract class ChatUser {
 	protected ChatMediator mediator;
+	protected ChatMenu UI;
 	protected String name;
 	protected int channel;
 	protected boolean is_agent;
 	
-	public ChatUser(ChatMediator med, String name, int channel, boolean is_agent) {
+	public ChatUser(ChatMediator med, ChatMenu menu, String name, int channel, boolean is_agent) {
 		this.mediator = med;
+		this.UI = menu
 		this.name = name;
 		this.channel = channel;
 		this.is_agent = is_agent;
